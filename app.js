e978a924821891aaeb82206770f9a7e95bfdd935
Shubham Dandge <shubham.dandge@Linkcode.in>
2023-07-04 20:16:52 +0530

    Protecting API

--- app.js
+++ app.js
@@ -1,25 +1,113 @@
 const express = require('express');
 const app = express();
-
 const { mongoose } = require('./db/mongoose');
 
 
 const bodyParser = require('body-parser');
 
 //Load in the mongoose models
-const { List, Task } = require('./db/models');
+const { List, Task, User } = require('./db/models');
+const { verify } = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+
+/**
+ * MIDDLEWARE
+ */
 
 
 //Load Middleware
 app.use(bodyParser.json());
 
 //CORS HEADERS MIDDLEWARE
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Methods","GET,POST, HEAD,OPTIONS,PUT, PATCH, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
+
+    res.header(
+        'Access-Control-Expose-Headers',
+        'x-access-token, x-refresh-token'
+    );
+
     next();
-  });
+});
+// check whether the request has a valid JWT access token
+let authenticate = (req, res, next) => {
+    let token = req.header('x-access-token');
+
+    // verify the JWT
+    jwt.verify(token, User.getJWTSecret(), (err, decoded) => {
+        if (err) {
+            // there was an error
+            // jwt is invalid - * DO NOT AUTHENTICATE *
+            res.status(401).send(err);
+        } else {
+            // jwt is valid
+            req.user_id = decoded._id;
+            next();
+        }
+    });
+}
+
+// Verify Refresh Token Middleware (which will be verifying the session)
+let verifySession = (req, res, next) => {   
+    // grab the refresh token from the request header
+    let refreshToken = req.header('x-refresh-token');
+
+    // grab the _id from the request header
+    let _id = req.header('_id');
+
+    User.findByIdAndToken(_id, refreshToken).then((user) => {
+        if (!user) {
+            // user couldn't be found
+            return Promise.reject({
+                'error': 'User not found. Make sure that the refresh token and user id are correct'
+            });
+        }
+
+
+        // if the code reaches here - the user was found
+        // therefore the refresh token exists in the database - but we still have to check if it has expired or not
+
+        req.user_id = user._id;
+        req.userObject = user;
+        req.refreshToken = refreshToken;
+
+        let isSessionValid = false;
+
+        user.sessions.forEach((sessions) => {
+            if (sessions.token === refreshToken) {
+                console.log("62")
+                // check if the session has expired
+                if (User.hasRefreshTokenExpired(sessions.expiresAt) === false) {
+                    console.log("hii");
+                    // refresh token has not expired
+                    isSessionValid = true;
+                }
+            }
+        });
+
+        if (isSessionValid) {
+            // the session is VALID - call next() to continue with processing this web request
+            console.log("success");
+            next();
+        } else {
+            // the session is not valid
+            return Promise.reject({
+                'error': 'Refresh token has expired or the session is invalid'
+            })
+        }
+
+    }).catch((e) => {
+        res.status(401).send(e);
+    })
+}   
+
+
+  /**End  MIDDLEWARE */
+
+
+
 
 /* Route Handlers */
 
@@ -28,9 +116,11 @@ app.use(function(req, res, next) {
  * GET /lists
  * Purpose : Get all Lists
  */
-app.get('/lists',(req, res) => {
-   // we want to return any array of all the lists in the database
-   List.find({}).then((lists)=>{
+app.get('/lists', authenticate, (req, res) => {
+   // we want to return any array of all the lists that belong to the authenticated user
+   List.find({
+  //  _userId:req.user_id
+   }).then((lists)=>{
     res.send(lists); 
 
    });
@@ -70,8 +160,12 @@ app.delete('/lists/:id',(req, res) => {
         _id: req.params.id
     }).then((removedListDoc) => {
         res.send(removedListDoc);
+
+        //delete all tasks that are in the deleted list
+        deleteTasksFromList(removedListDoc._id);
     })
 });
+
 /**
  * GET/lists/:listId/tasks
  * Purpose: Get all tasks in a specific list
@@ -136,7 +230,105 @@ app.delete('/lists/:listId/tasks/:taskId',(req, res) =>{
         res.send(removedTaskDoc);
     })
 });
+
+
+
+
+
+/** USER ROUTES */
+/**
+ * POST /users
+ * Purpose: Sign up
+ * 
+ */
+app.post('/users', (req, res) => {
+    //User sign up
+
+    let body = req.body;
+    let newUser = new User(body);
+    newUser.save().catch(err => {
+        console.log('err', err)
+
+    }).then((res) =>{
+        return newUser.createSession();
+    }).then((refreshToken) => {
+        console.log('refreshToken', refreshToken)
+        //Session created sucessfully - refreshToken returned.
+        //now we generate an access auth token for the user
+
+        return newUser.generateAccessAuthToken().then((accessToken) => {
+            console.log('accessToken 167', accessToken)
+            //access auth token generated sucessfully, now we return an object containing the auth tokens
+            return { accessToken, refreshToken}
+        });
+    }).then((authTokens) => {
+        //Now we construct and send the response to the user with their auth token in the header and th user object in the body
+        res
+            .header('x-refresh-token', authTokens.refreshToken)
+            .header('x-access-token', authTokens.accessToken)
+            .send(newUser);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
+
+/**
+ * POST /users/login
+ * Purpose : Login
+ * 
+ */
+app.post('/users/login',(req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.findByCredentials(email, password).then((user) =>{
+        console.log('email', email)
+        return user.createSession().then((refreshToken)=>{
+            //session created sucessfully -refreshToken
+            //now we generate an access auth token for the user
+
+            return user.generateAccessAuthToken().then((accessToken) =>{
+                //access auth token generated sucessfully, now we return  an object containing the auth token
+                return { accessToken, refreshToken}
+
+            }).catch(err => err);
+        }).then((authTokens)=>{
+        //Now we construct and send the response to the user with their auth token in the header and th user object in the body
+        res
+            .header('x-refresh-token', authTokens.refreshToken)
+            .header('x-access-token', authTokens.accessToken)
+            .send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+
+})
+
+/**Helper */
+let deleteTasksFromList = (_listId) => {
+    Task.deleteMany({
+        _listId
+    }).then(() => {
+        console.log("Tasks from " + _listId + " were deleted!");
+    }) 
+}
 app.listen(3000,() => {
     console.log("Server is listening on port 3000");
     
 });
+
+/**
+ * GET /users/me/access-token
+ * Purpose: generates and returns an access token
+ */
+app.get('/users/me/access-token', verifySession, (req, res) =>{
+    //we know that the user caller is authenticated and we have the user id and user object available to us
+    req.userObject.generateAccessAuthToken().then((accessToken) => {
+        res.header('x-access-token',accessToken).send({ accessToken }); 
+        console.log("296");
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+
+})
---
+++ db/models/user.model.js
@@ -0,0 +1,185 @@
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const e = require('express');
+const bcrypt = require('bcryptjs');
+//JWT Secret
+const jwtSecret = "43390190916338987150fsdklafjjhcf2134414359";
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength:8
+    },
+    sessions:[{
+        token:{
+            type:String,
+            required:true,
+
+        },
+        expiresAt:{
+            type: Number,
+            required: true
+        }
+    }]
+});
+
+//**Instance Methods */
+UserSchema.methods.toJSON = function(){
+    const user = this;
+    const userObject = user.toObject();
+
+    //return the documents except the password and sessions(these shouldn't be made availabe)
+    return _.omit(userObject, ['password','sessions']);
+}
+UserSchema.methods.generateAccessAuthToken = function(){
+    const user = this;
+    return new Promise((resolve, reject) =>{
+        //create the JSON web Token and return that
+        jwt.sign({ _id: user._id.toHexString()}, jwtSecret,{ expiresIn:"15m"}, (err, token) =>{
+            if (!err){
+                resolve(token);
+            }
+            else{
+                // there is an error
+                reject(err);
+            }
+        })
+    })
+}
+UserSchema.methods.generatesRefreshAuthToken = function(){
+    //THis method simply generates a 64byte hex string - it doesnt save it to the database. SaveSessionToDatabase() does that.
+    return new Promise((resolve, reject) => {
+        crypto.randomBytes(64,(err,buf)=>{
+            if(!err){
+                // no error
+                let token = buf.toString('hex');
+
+                return resolve(token);
+            } else {
+                reject(err);
+            }
+        })
+
+
+    })
+}
+
+UserSchema.methods.createSession = function(){
+    let user =this;
+    console.log('user', user)
+
+    return user.generatesRefreshAuthToken().then((refreshToken) =>{
+        console.log('refreshToken 79', refreshToken)
+        return saveSessionToDatabase(user,refreshToken);
+    }).then((refreshToken)=>{
+        console.log('refreshToken 81', refreshToken)
+        //saved to dtabase sucessfully
+        //now return the refresh token
+        return refreshToken;
+    }).catch((e)=>{
+        console.log('e', e)
+        return promiseHooks.reject('Failed to save session to database. \n' +e);
+
+    })
+}
+/*MODEL METHODS (static methods) */
+
+UserSchema.statics.getJWTSecret = () => {
+    return jwtSecret;
+}
+
+UserSchema.statics.findByIdAndToken = function(_id, token){
+    //finds user by id and token
+    //used in auth middleware (verifySession)
+    const User = this;
+    return User.findOne({
+        _id,
+        'sessions.token':token
+    });
+}
+UserSchema.statics.findByCredentials = function(email, password){
+    let user = this;
+    return User.findOne({ email }).then((user) =>{
+        if (!user) return Promise.reject();
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password,user.password, (err, res) => {
+                if (res) resolve (user);
+                else{
+                    reject();
+                }
+            })
+        }).catch(err => err)
+    })
+
+}
+
+UserSchema.statics.hasRefreshTokenExpired = (expiresAt) => {
+    let secondsSinceEpoch = Date.now() / 1000;
+    if (expiresAt > secondsSinceEpoch ){
+        //hasn't expired
+        return false;
+    }
+    else{
+        //has expired
+        return true;
+    }
+}
+
+/*MIDDLEWARE */
+//Before a user document is saved , this code runs
+UserSchema.pre('save',function(next){
+    let user = this;
+    let costFactor=10;
+
+
+    if(user.isModified('password')){
+        //if the password field  has been edited/changed thenrun thiscode.
+        //Generate salt and hash password
+        bcrypt.genSalt(costFactor,(err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) =>{
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+
+});
+/*HELPER METHODS */
+let saveSessionToDatabase = (user, refreshToken) =>{
+    // save session to database
+    return new Promise((resolve, reject) => {
+        let expiresAt = generatesRefreshTokenExpiryTime();
+        console.log('expiresAt', expiresAt)
+
+        user.sessions.push({'token' : refreshToken, expiresAt });
+
+        user.save().then((res)=>{
+            console.log('res 161', res) 
+            //saved session sucessfully
+            return resolve(refreshToken);
+        }).catch((e)=>{
+            reject(e);
+        });
+    })
+}
+let generatesRefreshTokenExpiryTime = () =>{
+    let daysUntilExpire= "10";
+    let secondsUntilExpire = ((daysUntilExpire * 24) *60) *60;
+    return((Date.now()/1000)+ secondsUntilExpire);
+}
+
+const User = mongoose.model('User',UserSchema);
+module.exports = { User }
